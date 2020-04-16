480ca094bfbf6c64705fcaf4c9120635a4ddda57
htsukahara <tsukahara.hirokazu@ajax-labo.com>
2020-04-16 16:30:51 +0900

    10_Authentication Provider

---
+++ lib/providers/auth_provider.dart
@@ -0,0 +1,33 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+
+enum AuthStatus {
+  NotAuthenticated,
+  Authenticating,
+  Authenticated,
+  UserNotFound,
+  Error,
+}
+
+class AuthProvider extends ChangeNotifier {
+  static AuthProvider instance = AuthProvider();
+  FirebaseAuth _auth;
+  AuthStatus status;
+  FirebaseUser user;
+
+  AuthProvider() {
+    _auth = FirebaseAuth.instance;
+  }
+
+  void loginUserWithEmailAndPassword(String _email, String _password) async {
+    status = AuthStatus.Authenticating;
+    notifyListeners();
+    try {
+      AuthResult _result = await _auth.signInWithEmailAndPassword(
+          email: _email, password: _password);
+      user = _result.user;
+      //Navigate to HomePage
+      print("Logged In Successfully on$user");
+    } catch (e) {}
+  }
+}
