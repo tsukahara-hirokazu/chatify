e1c9107e607401bccf23ad32a483524809c41031
htsukahara <tsukahara.hirokazu@ajax-labo.com>
2020-04-20 12:03:37 +0900

    16_Registraion Page UI part2

--- lib/pages/registration_page.dart
+++ lib/pages/registration_page.dart
@@ -20,16 +20,18 @@ class _RegistrationPageState extends State<RegistrationPage> {
     _deviceWidth = MediaQuery.of(context).size.width;
     _deviceHeight = MediaQuery.of(context).size.height;
 
-    return Scaffold(
-      backgroundColor: Theme.of(context).backgroundColor,
-      body: Container(
-        alignment: Alignment.center,
-        child: _signupPageUI(),
+    return SafeArea(
+      child: Scaffold(
+        backgroundColor: Theme.of(context).backgroundColor,
+        body: Container(
+          alignment: Alignment.center,
+          child: _registrationPageUI(),
+        ),
       ),
     );
   }
 
-  Widget _signupPageUI() {
+  Widget _registrationPageUI() {
     return Container(
       height: _deviceHeight * 0.75,
       padding: EdgeInsets.symmetric(horizontal: _deviceWidth * 0.1),
@@ -40,6 +42,8 @@ class _RegistrationPageState extends State<RegistrationPage> {
         children: <Widget>[
           _headingWidget(),
           _inputForm(),
+          _registerButton(),
+          _backToLoginButton(),
         ],
       ),
     );
@@ -79,14 +83,17 @@ class _RegistrationPageState extends State<RegistrationPage> {
           mainAxisSize: MainAxisSize.max,
           crossAxisAlignment: CrossAxisAlignment.start,
           children: <Widget>[
-            imageSelectorWidget(),
+            _imageSelectorWidget(),
+            _nameTextField(),
+            _emailTextField(),
+            _passwordTextField(),
           ],
         ),
       ),
     );
   }
 
-  Widget imageSelectorWidget() {
+  Widget _imageSelectorWidget() {
     return Align(
       alignment: Alignment.center,
       child: Container(
@@ -100,6 +107,89 @@ class _RegistrationPageState extends State<RegistrationPage> {
             image: NetworkImage("https://dic.nicovideo.jp/oekaki/334039.png"),
           ),
         ),
+      ),
+    );
+  }
+
+  Widget _nameTextField() {
+    return TextFormField(
+      autocorrect: false,
+      style: TextStyle(color: Colors.white),
+      validator: (_input) {
+        return _input.length != 0 ? null : "名前を入力してください";
+      },
+      onSaved: (_input) {},
+      cursorColor: Colors.white,
+      decoration: InputDecoration(
+        hintText: "名前",
+        focusedBorder: UnderlineInputBorder(
+          borderSide: BorderSide(color: Colors.white),
+        ),
+      ),
+    );
+  }
+
+  Widget _emailTextField() {
+    return TextFormField(
+      autocorrect: false,
+      style: TextStyle(color: Colors.white),
+      validator: (_input) {
+        return _input.length != 0 && _input.contains("@")
+            ? null
+            : "適切なメールアドレスを入力してください";
+      },
+      onSaved: (_input) {},
+      cursorColor: Colors.white,
+      decoration: InputDecoration(
+        hintText: "メールアドレス",
+        focusedBorder: UnderlineInputBorder(
+          borderSide: BorderSide(color: Colors.white),
+        ),
+      ),
+    );
+  }
+
+  Widget _passwordTextField() {
+    return TextFormField(
+      autocorrect: false,
+      obscureText: true,
+      style: TextStyle(color: Colors.white),
+      validator: (_input) {
+        return _input.length != 0 ? null : "適切なパスワードを入力してください";
+      },
+      onSaved: (_input) {},
+      cursorColor: Colors.white,
+      decoration: InputDecoration(
+        hintText: "パスワード",
+        focusedBorder: UnderlineInputBorder(
+          borderSide: BorderSide(color: Colors.white),
+        ),
+      ),
+    );
+  }
+
+  Widget _registerButton() {
+    return Container(
+      height: _deviceHeight * 0.06,
+      width: _deviceWidth,
+      child: MaterialButton(
+        onPressed: () {},
+        color: Colors.blue,
+        child: Text(
+          "会員登録",
+          style: TextStyle(fontSize: 14, fontWeight: FontWeight.w700),
+        ),
+      ),
+    );
+  }
+
+  Widget _backToLoginButton() {
+    return GestureDetector(
+      onTap: () {},
+      child: Container(
+        height: _deviceHeight * 0.06,
+        width: _deviceWidth,
+        child: Icon(Icons.arrow_back, size: 30),
       ),
     );
   }
